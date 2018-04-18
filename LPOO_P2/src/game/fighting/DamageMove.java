f49bab9f2870da7bc6c454b9217b4a4a9264a751
Tiago Barbosa <tiagobarbosa98@gmail.com>
2018-04-18 11:01:15 +0100

    Further work on DamageMove class

--- LPOO_P2/src/game/fighting/DamageMove.java
+++ LPOO_P2/src/game/fighting/DamageMove.java
@@ -52,22 +52,22 @@ public class DamageMove {
 	
 	
 	//How much damage the move from Fighter f1 will do (after calculations) to Fighter f2
-		public double dmgOutput(Fighter f1, Fighter f2)
-		{		
-			//modified crit chance to help with calcs
-			double modCrit = critChance / 100.0;
-			
-			//random damage modifier
-			double random = u.numberBetween(0.85, 1.0);
-			
-			double damageDone = power * f1.getAtk() / f2.getDef();
-			
-			//if crit chance is successful, damage done will be increased by 150%
-			if(Math.random() <= modCrit)
-				return (damageDone) * random * 1.5;
-			else
-				return (damageDone) * random;
-					
-		}
+	public double dmgOutput(Fighter f1, Fighter f2)
+	{		
+		//modified crit chance to help with calcs
+		double modCrit = critChance / 100.0;
+
+		//random damage modifier
+		double random = u.numberBetween(0.85, 1.0);
+
+		double damageDone = power * f1.getAtk() / f2.getDef();
+
+		//if crit chance is successful, damage done will be increased by 150%
+		if(Math.random() <= modCrit)
+			return (damageDone) * random * 1.5;
+		else
+			return (damageDone) * random;
+
+	}
 	
 }
